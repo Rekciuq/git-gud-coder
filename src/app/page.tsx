e5e4b8da8dac65729ae88ff544f187baa2f45b41
Rekciuq <danilaka76@outlook.com>
2025-07-27 16:05:11 +0300

    updated root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,5 @@
-"use client";
-
-import { useUserStore } from "@/context/UserProvider";
-import { useTRPC } from "@/lib/trpc/client/client";
-import { logoutAction } from "@/serverActions/logout";
-import ToastEmitter from "@/services/client/ToastEmitter";
-import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const tRPC = useTRPC();
-
-  const updateUser = useUserStore((state) => state.updateUser);
-  const user = useUserStore((state) => state.user);
-
-  const getMeOptions = tRPC.user.getUser.queryOptions();
-  const { data: userData } = useQuery({ ...getMeOptions, initialData: user });
-
-  useEffect(() => {
-    if (!userData) return;
-
-    updateUser(userData);
-  }, [userData, updateUser]);
-
-  return (
-    <>
-      <button
-        onClick={() => {
-          logoutAction();
-          ToastEmitter.success("Logged out!");
-        }}
-      >
-        Im here
-      </button>
-      <p>{user?.email}</p>
-      <p>{user?.firstName}</p>
-    </>
-  );
+  redirect("/dashboard");
 }
